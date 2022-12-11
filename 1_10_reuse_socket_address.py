fc9960b67fe3a1bd3ee4b9b33dff325395d0e85f
hyo-jae-jung <hyojaejung@kakao.com>
2022-12-12 01:51:20 +0900

    network programming chapter 1

---
+++ 1_10_reuse_socket_address.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+def reuse_socket_addr():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Get the old state of the SO_REUSEADDR option
+    old_state = sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+    print(f"Old sock state: {old_state}")
+
+    # Enable the SO_REUSEADDR option
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    new_state = sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR)
+    print(f"New sock state: {new_state}")
+
+    local_port = 8282
+
+    srv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    srv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    srv.bind(('', local_port))
+    srv.listen(1)
+    print(f"Listening on port: {local_port}")
+    while True:
+        try:
+            connection, addr = srv.accept()
+            print(f"Connected by {addr[0]}:{addr[1]}")
+        except KeyboardInterrupt:
+            break
+        except socket.error as msg:
+            print(f"{msg}")
+
+if __name__ == "__main__":
+    reuse_socket_addr()
+    
